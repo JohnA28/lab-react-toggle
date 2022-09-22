7e94f7ccccb8e06d471af6d20fbe15d7370702cd
JohnA28 <John.Alban41@gmail.com>
2022-09-22 17:02:23 -0400

    rendered 5 boards, added comments for clarity

--- src/components/BoardSwitcher.js
+++ src/components/BoardSwitcher.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Board(props) {
   let className = "board";
@@ -10,18 +10,39 @@ function Board(props) {
 }
 
 function BoardSwitcher(props) {
+/*
+this piece of code below creates an array called boards
+populates the array based on numBoards which is set in 
+index.js file, isSelected controls which box is highlighted.
+
+
+to solve this lab I need to have isSelected iteriate through the array as the 
+user clicks toggle and when they reach the end of the array it loops back to
+the start. I know I can use useState to help iterate, need to check out recording 
+from last lecture.
+*/
   let boards = [];
   for (let ii = 0; ii < props.numBoards; ii++) {
-    let isSelected = ii === 0;
+    let isSelected = ii === 0; // try to edit this piece of code to get desired affect
     boards.push(<Board index={ii} selected={isSelected} key={ii} />);
+  }
+
+  /* 
+  the function below is going to be an the event handler for the toggle button
+  whenever the toggle button is clicked by the user it will highlight the next 
+  box in line.
+  */
+
+  const handleClick = (event) => {
+    console.log("this button was pressed");
   }
 
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button>Toggle</button>
+      <button onClick={handleClick}>Toggle</button>  
     </div>
-  );
+  );   // button will use handleClick function to keep track of when button is pressed 
 }
 
 export default BoardSwitcher;
